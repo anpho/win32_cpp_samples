2a7eed1904bf5e8c55beb8f8d0e0d20af18724ed
Merrick Zhang <anphorea@gmail.com>
2021-12-11 14:47:25 +0800

    补充注释

--- shell/enter_file_rename_mode.cpp
+++ shell/enter_file_rename_mode.cpp
@@ -1,3 +1,8 @@
+/*
+示例：让资源管理器选中文件并进入重命名状态
+Merrick Zhang (anphorea@gmail.com)
+*/
+
 #include <iostream>
 #include <Windows.h>
 #include <atlbase.h>
@@ -7,11 +12,16 @@
 
 int main()
 {
+	//初始化COM操作
 	CoInitialize(NULL);
+	//打开个explorer窗口
 	ShellExecute(NULL, L"open", L"c:\\users\\default", NULL, NULL, SW_SHOWDEFAULT);
+	//2秒，等它打开
 	Sleep(2000);
 
 	ATL::CString folder_to_select(L"C:\\Users\\default\\Downloads");
+	
+	//以下代码的逻辑部分来自Raymond Chen，具体代码使用CComPtr/IID_PPV_ARGS等方式进行了调整
 	CComPtr<IShellWindows> shellwindow;
 	HRESULT h = CoCreateInstance(CLSID_ShellWindows, NULL, CLSCTX_ALL, IID_PPV_ARGS(&shellwindow));
 	if (FAILED(h)) { return 0; }
@@ -52,6 +62,7 @@ int main()
 									fpath.Canonicalize();
 									if (fpath.m_strPath.CompareNoCase(folder_to_select) == 0) {
 										try {
+											//使用SVSI_EDIT标志进入编辑状态
 											if (SUCCEEDED(pfv->SelectItem(i, SVSI_EDIT | SVSI_SELECT | SVSI_DESELECTOTHERS | SVSI_ENSUREVISIBLE))) {
 											}
 										}
@@ -68,6 +79,7 @@ int main()
 			}
 		}
 	}
+	//清理COM
 	CoUninitialize();
 
 }
